979a05482fe5778d431a3e0b04db5a93ba66613f
SomeClown <justsomeclown@gmail.com>
2018-04-01 13:26:24 -0700

    adding some rudimentary error handling (try/except/assert/raise)

--- do2.py
+++ do2.py
@@ -32,7 +32,8 @@ TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
 
-api_token = ''
+with open('uid.txt', 'r') as f:
+    api_token = (f.readline().rstrip())
 api_url_base = 'https://api.digitalocean.com/v2/'
 headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer {0}'.format(api_token)}
 
@@ -49,9 +50,6 @@ color_purple2 = "\033[1;35m"
 color_cyan2 = "\033[1;36m"
 color_white2 = "\033[1;37m"
 color_off = "\33[00m"
-
-with open('uid.txt', 'r') as f:
-    api_token = (f.readline().rstrip())
 
 
 @click.group()
@@ -73,22 +71,23 @@ def get_stuff(suffix: str):
     if response.status_code == 200:
         return json.loads(response.content.decode('utf-8'))
     elif response.status_code >= 500:
-        print('[!] [{0}] Server Error'.format(response.status_code))
+        print(color_red2_on + '\n[!] [{0}] Server Error'.format(response.status_code))
         return None
     elif response.status_code == 404:
-        print('[!] [{0}] URL not found: [{1}]'.format(response.status_code, api_url))
+        print(color_red2_on + '\n[!] [{0}] URL not found: [{1}]'.format(response.status_code, api_url))
         return None
     elif response.status_code == 401:
-        print('[!] [{0}] Authentication Failed'.format(response.status_code))
+        print(color_red2_on + '\n[!] [{0}] Authentication Failed'.format(response.status_code))
         return None
     elif response.status_code == 400:
-        print('[!] [{0}] Bad Request'.format(response.status_code))
+        print(color_red2_on + '\n[!] [{0}] Bad Request'.format(response.status_code))
         return None
     elif response.status_code >= 300:
-        print('[!] [{0}] Unexpected Redirect'.format(response.status_code))
+        print(color_red2_on + '\n[!] [{0}] Unexpected Redirect'.format(response.status_code))
         return None
     else:
-        print('[?] Unexpected Error: [HTTP {0}]: Content: {1}'.format(response.status_code, response.content))
+        print(color_red2_on + '\n[?] Unexpected Error: [HTTP {0}]: '
+                              'Content: {1}'.format(response.status_code, response.content))
         return None
 
 
@@ -98,6 +97,8 @@ def return_account_info():
     Returns account information
     """
     get_account = get_stuff(suffix='account')
+    if not isinstance(get_account, dict):
+        raise TypeError('returned object type is incorrect')
     click.echo()
     for k, v in get_account['account'].items():
         print(color_red2_on + '{:45}'.format(k) + color_red2_off + ':' + '{0}'.format(v))
@@ -110,6 +111,8 @@ def return_images():
     Returns list of available images
     """
     get_images = get_stuff(suffix='images')
+    if not isinstance(get_images, dict):
+        raise TypeError('returned object type is incorrect')
     click.echo()
 
     def print_dict(d: dict):
@@ -119,8 +122,7 @@ def return_images():
         :return: 
         """
         for k, v in d.items():
-            if isinstance(v, dict):
-                # print(color_red2_on + '{0}: '.format(k))
+            if isinstance(v, dict):                # print(color_red2_on + '{0}: '.format(k))
                 print_dict(v)
             elif isinstance(v, list):
                 for item in v:
@@ -128,7 +130,6 @@ def return_images():
                           color_red2_on + 'Distribution: ' + color_red2_off + item['distribution'])
             else:
                 click.echo()
-                # print(color_red2_on + '{0}'.format(k) + color_red2_off + ': {0}'.format(v))
     print_dict(get_images)
 
 
@@ -143,8 +144,9 @@ def return_droplets(droplet_id: int):
         print(color_red2_on + '\nNot yet implemented\n' + color_off)
         return
     else:
-        click.echo()
         get_droplets = get_stuff(suffix='droplets')
+        if not isinstance(get_droplets, dict):
+            raise TypeError('returned object is incorrect')
 
     def print_dict(d: dict):
         """
@@ -152,9 +154,9 @@ def return_droplets(droplet_id: int):
         :param d: 
         :return: 
         """
+        click.echo()
         for k, v in d.items():
             if isinstance(v, dict):
-                # print(color_red2_on + '{0}: '.format(k))
                 print_dict(v)
             elif isinstance(v, list):
                 for item in v:
@@ -184,7 +186,6 @@ def return_droplets(droplet_id: int):
                     print(color_red2_on + 'Tags: ' + color_red2_off + str(item['tags']))
             else:
                 click.echo()
-                # print(color_red2_on + '{0}'.format(k) + color_red2_off + ': {0}'.format(v))
 
     print_dict(get_droplets)
 
@@ -195,14 +196,20 @@ def return_ssh_keys():
     Returns ssh keys information
     """
     get_account = get_stuff(suffix='account/keys')
-    click.echo()
-    print(color_red2_on + 'SSH Key Information:' + color_off)
-    for item in get_account['ssh_keys']:
-        print(color_blue2 + '\tID: ' + color_off + str(item['id']))
-        print(color_blue2 + '\tName: ' + color_off + str(item['name']))
-        print(color_blue2 + '\tFingerprint: ' + color_off + str(item['fingerprint']))
-        print(color_blue2 + '\tPublic Key: ' + color_yellow2 + '{0}'.format(str(item['public_key'])))
-    click.echo()
+    if not isinstance(get_account, dict):
+        raise TypeError('returned object type is incorrect')
+    try:
+        click.echo()
+        print(color_red2_on + 'SSH Key Information:' + color_off)
+        for item in get_account['ssh_keys']:
+            print(color_blue2 + '\tID: ' + color_off + str(item['id']))
+            print(color_blue2 + '\tName: ' + color_off + str(item['name']))
+            print(color_blue2 + '\tFingerprint: ' + color_off + str(item['fingerprint']))
+            print(color_blue2 + '\tPublic Key: ' + color_yellow2 + '{0}'.format(str(item['public_key'])))
+        click.echo()
+    except BaseException as e:
+        print(e)
+        click.echo()
 
 cli_init.add_command(return_account_info, 'account')
 cli_init.add_command(return_images, 'images')
