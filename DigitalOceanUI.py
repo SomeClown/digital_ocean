c9186126622dc5cee51a0574156802ecb63d3a8c
SomeClown <justsomeclown@gmail.com>
2018-04-02 22:50:48 -0700

    adding license information

--- DigitalOceanUI.py
+++ DigitalOceanUI.py
@@ -6,6 +6,34 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 import requests
 import json
 
+__author__ = "SomeClown"
+__license__ = "MIT"
+__maintainer__ = "Teren Bryson"
+__email__ = "teren@packetqueue.net"
+
+"""
+Copyright 2018 by Teren Bryson
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
 
 class Ui_MainWindow(object):
     """
@@ -82,7 +110,6 @@ class Ui_MainWindow(object):
                'Authorization': 'Bearer {0}'.format(api_token)}
 
     def teren(self):
-        #self.textBrowser.setPlainText('test')
         self.return_account_info()
 
     def get_stuff(self, suffix: str):
